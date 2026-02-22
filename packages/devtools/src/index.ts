d6c97c4ca858c41cd93704669bcf2f45cabd54d4
NaiKiDEV <pc999111@gmail.com>
2026-02-22 21:43:29 +0200

    feat(devtools): wire up package exports

--- packages/devtools/src/index.ts
+++ packages/devtools/src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export { createDevtools } from "./devtools";
+export { EventCollector } from "./collector";
+export { windowMessageTransport, memoryTransport } from "./transport";
+export type {
+  TimelineEntry,
+  StateSnapshot,
+  DevtoolsMessage,
+  Transport,
+  DevtoolsOptions,
+} from "./types";
